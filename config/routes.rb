7009e1698089ea1a61e13fd61c10d9443d1c32b2
sango735 <735th.floor@gmail.com>
2021-01-11 14:04:58 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   #ユーザー編集に必要なルーティングを追記する。
   #その後コマンドでcontrollerを作成する
   resources :rooms, only: [:new, :create]
+  resources :rooms,only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
+  
 end
