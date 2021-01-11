ad2d8343b464649f8034032821acbe7bfbaca086
sango735 <735th.floor@gmail.com>
2021-01-11 12:22:10 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   #ユーザー編集に必要なルーティングを追記する。
   #その後コマンドでcontrollerを作成する
